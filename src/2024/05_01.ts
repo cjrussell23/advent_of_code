922f2c32e2461f5c05c3c06e987dddd1c23f4139
Charles Russell <chjrussell@gmail.com>
2025-05-10 22:28:20 -0600

    day 5

---
+++ src/2024/05_01.ts
@@ -0,0 +1,28 @@
+import { readInput } from "../utils";
+
+const input = readInput('05')
+const sections = input.split('\n\n');
+
+const rules = sections[0].split('\n').map(line => line.split('|').map(Number));
+const instructions = sections[1].split('\n').map(line => line.split(',').map(Number));
+
+let total = 0;
+for (const set of instructions) {
+  const sortedSet = [...set];
+  sortedSet.sort((a, b) => {
+    const rule = rules.find(rule => rule.includes(a) && rule.includes(b));
+    if (rule) {
+      const indexA = rule.indexOf(a);
+      const indexB = rule.indexOf(b);
+      return indexA - indexB;
+    }
+    return 0;
+  });
+  const eq = sortedSet.every((value, index) => value === set[index]);
+  if (eq) {
+    const center = set[Math.floor(set.length / 2)];
+    total += center;
+  }
+}
+
+console.log(`Total: ${total}`);
